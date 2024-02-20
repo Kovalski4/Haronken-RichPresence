380c3129835b2a75af6acb69a36a6fe8af31e46e
Murat YILMAZ <murattnt@live.com>
2024-02-20 17:11:47 +0300

    loooooooool

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const client = new Client();
-x = 0;
+super_mantikli_kodlama_pratigi = 0;
 
 client.on('ready', async () => {
   updatePresence();
@@ -15,24 +15,24 @@ async function updatePresence() {
     'https://i.ibb.co/x6L5BVX/removal-ai-b65445d5-96ed-43fa-8ada-886e26b0c06a-logo-removebg-preview-nsklln3na-transformed.png', // Required if the image you use is not in Discord
   );
 
-  if (x = 0) {
-    x = 1;
+  if (super_mantikli_kodlama_pratigi = 0) {
+    super_mantikli_kodlama_pratigi = 1;
   } else {
-    x = 0;
+    super_mantikli_kodlama_pratigi = 0;
   }
 
   const status = new RichPresence(client)
     .setApplicationId('367827983903490050')
     .setType('STREAMING')
     .setURL('https://www.twitch.tv/haronken')
-    .setState('Ekonomi Vanilla Deneyimi! [1.20.4]')
+    .setState('Ekonomi Vanilla Deneyimi! [1.20.4]'+super_mantikli_kodlama_pratigi)
     .setName('Minecraft')
     .setDetails('oyna.haronken.com.tr')
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(getExtendURL[0].external_asset_path)
     .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz!')
     .setAssetsSmallImage('367827983903490050') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
-    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!'+x)
+    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!'+super_mantikli_kodlama_pratigi)
     .addButton('Discord', 'https://discord.gg/Gp6GTsCyAr');
 
   client.user.setPresence({ activities: [status] });
