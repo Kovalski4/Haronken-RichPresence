dbb1cd6842ff19e78615d82cbcb84368a6a5a23b
Murat YILMAZ <murattnt@live.com>
2024-02-20 16:52:30 +0300

    bu kodu okuyanlara sa XD

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const client = new Client();
 
+const getExtendURL = null;
+x = 0;
+
 client.on('ready', async () => {
-  const getExtendURL = await RichPresence.getExternal(
+  getExtendURL = await RichPresence.getExternal(
     client,
     '367827983903490050',
     'https://i.ibb.co/x6L5BVX/removal-ai-b65445d5-96ed-43fa-8ada-886e26b0c06a-logo-removebg-preview-nsklln3na-transformed.png', // Required if the image you use is not in Discord
   );
 
+  updatePresence();
+
+});
+
+client.setInterval(() => {
+  if (x = 0) {
+    x = 1;
+  } else {
+    x = 0;
+  }
+  updatePresence();
+}, 1000 * 60 * 10);
+
+function updatePresence() {
   const status = new RichPresence(client)
     .setApplicationId('367827983903490050')
     .setType('STREAMING')
@@ -19,12 +36,12 @@ client.on('ready', async () => {
     .setAssetsLargeImage(getExtendURL[0].external_asset_path)
     .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz!')
     .setAssetsSmallImage('367827983903490050') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
-    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!')
+    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!'+x)
     .addButton('Discord', 'https://discord.gg/Gp6GTsCyAr');
 
   client.user.setPresence({ activities: [status] });
 
-  client.user.setGlobalName('Haronken v2');
-});
+  client.user.setGlobalName('Haronken');
+}
 
 client.login(process.env.DISCORD_USER_TOKEN);
