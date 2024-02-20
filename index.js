fd0c39aafa41793823d28151019d91f06f0d740e
Murat YILMAZ <murattnt@live.com>
2024-02-20 18:31:04 +0300

    yuhbekrdsm

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ client.on('ready', async () => {
     .setDetails('oyna.haronken.com.tr')
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(getExtendURL[0].external_asset_path)
-    .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz'+super_mantikli_kodlama_pratigi)
+    .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz')
     .setAssetsSmallImage('367827983903490050') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
-    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz'+super_mantikli_kodlama_pratigi)
+    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz')
     .addButton('Discord', 'https://discord.gg/Gp6GTsCyAr');
 
   client.user.setPresence({ activities: [status] });
