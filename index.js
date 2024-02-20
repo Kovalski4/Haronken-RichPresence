b82978a32ab173f31697d0ce352da9d7a599af12
Murat YILMAZ <murattnt@live.com>
2024-02-20 13:23:19 +0300

    test6

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const client = new Client();
 
 client.on('ready', async () => {
+  const getExtendURL = await RichPresence.getExternal(
+    client,
+    '367827983903490050',
+    'https://i.ibb.co/ZWrgLD9/you-have-an-ugly-gray-creeper-instead-of-a-minecraft-icon-v0-y83ppc5i6r4b1.png', // Required if the image you use is not in Discord
+  );
+
   const status = new RichPresence(client)
     .setApplicationId('367827983903490050')
     .setType('PLAYING')
     .setURL('https://discord.gg/Gp6GTsCyAr')
-    .setState('Vanilla 1.20.4')
+    .setState('Ekonomi Vanilla Deneyimi! [1.20.4]')
     .setName('Minecraft')
     .setDetails('oyna.haronken.com.tr')
-    .setParty({
-      max: 60,
-      current: 29,
-    })
     .setStartTimestamp(Date.now())
     .setAssetsLargeText('Idle')
-    .setAssetsSmallImage('373370493127884800') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
+    .setAssetsSmallImage('367827983903490050') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
     .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!')
     .addButton('Discord', 'https://discord.gg/Gp6GTsCyAr');
 
