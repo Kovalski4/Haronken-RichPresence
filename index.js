b6f131c5b5856a1ac9b94dd0a01c00870d095b1c
Murat YILMAZ <murattnt@live.com>
2024-02-20 17:14:13 +0300

    ehetenandayo

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ async function updatePresence() {
     .setApplicationId('367827983903490050')
     .setType('STREAMING')
     .setURL('https://www.twitch.tv/haronken')
-    .setState('Ekonomi Vanilla Deneyimi! [1.20.4]'+super_mantikli_kodlama_pratigi)
+    .setState('Ekonomi Vanilla Deneyimi! [1.20.4]')
     .setName('Minecraft')
     .setDetails('oyna.haronken.com.tr')
     .setStartTimestamp(Date.now())
     .setAssetsLargeImage(getExtendURL[0].external_asset_path)
-    .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz!')
+    .setAssetsLargeText('BETA sürecinde seni de aramızda görmek isteriz!'+super_mantikli_kodlama_pratigi)
     .setAssetsSmallImage('367827983903490050') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
-    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!'+super_mantikli_kodlama_pratigi)
+    .setAssetsSmallText('BETA sürecinde seni de aramızda görmek isteriz!')
     .addButton('Discord', 'https://discord.gg/Gp6GTsCyAr');
 
   client.user.setPresence({ activities: [status] });
